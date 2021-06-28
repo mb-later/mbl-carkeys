487a06d53853d0756f72ea4c0a6e0817909ccc0c
Fatih <68826839+mb-later@users.noreply.github.com>
2021-06-29 02:32:13 +0300

    Add files via upload

---
+++ progressbar/html/js/script.js
@@ -0,0 +1,55 @@
+var cancelledTimer = null;
+
+$('document').ready(function() {
+    Progressbar = {};
+
+    Progressbar.Progress = function(data) {
+        clearTimeout(cancelledTimer);
+        $("#progress-label").text(data.label);
+
+        $(".progress-container").fadeIn('fast', function() {
+            $("#progress-bar").stop().css({"width": 0, "background-color": "#1787e2a6"}).animate({
+              width: '100%'
+            }, {
+              duration: parseInt(data.duration),
+              complete: function() {
+                $(".progress-container").fadeOut('fast', function() {
+                    $("#progress-bar").css("width", 0);
+                    $.post('https://progressbar/FinishAction', JSON.stringify({
+                        })
+                    );
+                })
+              }
+            });
+        });
+    };
+
+    Progressbar.ProgressCancel = function() {
+        $("#progress-label").text("İPTAL EDİLDİ");
+        $("#progress-bar").stop().css( {"width": "100%", "background-color": "rgba(71, 0, 0, 0.8)"});
+
+        cancelledTimer = setTimeout(function () {
+            $(".progress-container").fadeOut('fast', function() {
+                $("#progress-bar").css("width", 0);
+                $.post('https://progressbar/CancelAction', JSON.stringify({
+                    })
+                );
+            });
+        }, 1000);
+    };
+
+    Progressbar.CloseUI = function() {
+        $('.main-container').fadeOut('fast');
+    };
+    
+    window.addEventListener('message', function(event) {
+        switch(event.data.action) {
+        case 'progress':
+            Progressbar.Progress(event.data);
+            break;
+        case 'cancel':
+            Progressbar.ProgressCancel();
+            break;
+        }
+    });
+});
